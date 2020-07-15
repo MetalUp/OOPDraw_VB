175351f213a2cedfa79bfd6571f8d7bdfc498efd
Richard Pawson <rpawson@nakedobjects.net>
2020-07-15 16:03:59 +0100

    End of Polymorphism chapter

---
+++ OOPDraw.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class OOPDraw
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Canvas = New System.Windows.Forms.PictureBox()
+        Me.LineWidth = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Colour = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Shape = New System.Windows.Forms.ComboBox()
+        CType(Me.Canvas, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Canvas
+        '
+        Me.Canvas.BackColor = System.Drawing.SystemColors.Window
+        Me.Canvas.Location = New System.Drawing.Point(292, 13)
+        Me.Canvas.Name = "Canvas"
+        Me.Canvas.Size = New System.Drawing.Size(709, 602)
+        Me.Canvas.TabIndex = 0
+        Me.Canvas.TabStop = False
+        '
+        'LineWidth
+        '
+        Me.LineWidth.FormattingEnabled = True
+        Me.LineWidth.Items.AddRange(New Object() {"Thin", "Medium", "Thick"})
+        Me.LineWidth.Location = New System.Drawing.Point(22, 59)
+        Me.LineWidth.Name = "LineWidth"
+        Me.LineWidth.Size = New System.Drawing.Size(121, 28)
+        Me.LineWidth.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(22, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(84, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Line Width"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(22, 103)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Colour"
+        '
+        'Colour
+        '
+        Me.Colour.FormattingEnabled = True
+        Me.Colour.Items.AddRange(New Object() {"Red", "Green", "Blue"})
+        Me.Colour.Location = New System.Drawing.Point(26, 127)
+        Me.Colour.Name = "Colour"
+        Me.Colour.Size = New System.Drawing.Size(121, 28)
+        Me.Colour.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(26, 176)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(56, 20)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Shape"
+        '
+        'Shape
+        '
+        Me.Shape.FormattingEnabled = True
+        Me.Shape.Items.AddRange(New Object() {"Line", "Rectangle"})
+        Me.Shape.Location = New System.Drawing.Point(26, 200)
+        Me.Shape.Name = "Shape"
+        Me.Shape.Size = New System.Drawing.Size(121, 28)
+        Me.Shape.TabIndex = 6
+        '
+        'OOPDraw
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1013, 627)
+        Me.Controls.Add(Me.Shape)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Colour)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.LineWidth)
+        Me.Controls.Add(Me.Canvas)
+        Me.Name = "OOPDraw"
+        Me.Text = "OOPDraw"
+        CType(Me.Canvas, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Canvas As PictureBox
+    Friend WithEvents LineWidth As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Colour As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Shape As ComboBox
+End Class
---
+++ OOPDraw.vb
@@ -0,0 +1,76 @@
+﻿
+Public Class OOPDraw
+
+    Private currentPen As Pen = New Pen(Color.Black)
+    Private dragging As Boolean = False
+    Private startOfDrag As Point = Point.Empty
+    Private lastMousePosition As Point = Point.Empty
+    Private shapes As List(Of Object) = New List(Of Object)
+
+    Public Sub New()
+        InitializeComponent()
+        LineWidth.SelectedItem = "Medium"
+        Colour.SelectedItem = "Green"
+        Shape.SelectedItem = "Line"
+    End Sub
+
+    Private Sub Canvas_Paint(ByVal sender As Object, ByVal e As PaintEventArgs) Handles Canvas.Paint
+        Dim gr As Graphics = e.Graphics
+        For Each shape As Object In shapes
+            shape.Draw(gr)
+        Next
+    End Sub
+
+    Private Sub Canvas_MouseDown(ByVal sender As Object, ByVal e As MouseEventArgs) Handles Canvas.MouseDown
+        dragging = True
+        startOfDrag = e.Location
+        lastMousePosition = e.Location
+        Select Case Shape.Text
+            Case "Line"
+                shapes.Add(New Line(currentPen, e.X, e.Y))
+            Case "Rectangle"
+                shapes.Add(New Rectangle(currentPen, e.X, e.Y))
+        End Select
+    End Sub
+
+    Private Sub Canvas_MouseMove(ByVal sender As Object, ByVal e As MouseEventArgs) Handles Canvas.MouseMove
+        If dragging Then
+            Dim shape As Object = shapes.Last()
+            shape.GrowTo(e.X, e.Y)
+            lastMousePosition = e.Location
+            Refresh()
+        End If
+    End Sub
+
+    Private Sub Canvas_MouseUp(ByVal sender As Object, ByVal e As MouseEventArgs) Handles Canvas.MouseUp
+        dragging = False
+    End Sub
+
+    Private Sub LineWidth_SelectedIndexChanged(ByVal sender As Object, ByVal e As EventArgs) _
+        Handles LineWidth.SelectedIndexChanged
+        Dim width As Single = currentPen.Width
+        Select Case LineWidth.Text
+            Case "Thin"
+                width = 2.0F
+            Case "Medium"
+                width = 4.0F
+            Case "Thick"
+                width = 8.0F
+        End Select
+        currentPen = New Pen(currentPen.Color, width)
+    End Sub
+
+    Private Sub Colour_SelectedIndexChanged(ByVal sender As Object, ByVal e As EventArgs) Handles Colour.SelectedIndexChanged
+        Dim color As Color = currentPen.Color
+        Select Case Colour.Text
+            Case "Red"
+                color = Color.Red
+            Case "Blue"
+                color = Color.Blue
+            Case "Green"
+                color = Color.Green
+        End Select
+        currentPen = New Pen(color, currentPen.Width)
+    End Sub
+End Class
+
